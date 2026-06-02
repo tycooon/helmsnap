ffb11555b06116805e8fbbc11e39511ed52db37d
Yuri Smirnov <tycoooon@gmail.com>
2026-06-02 12:41:58 +0300

    fix: strip whitespace-only lines before blank line normalization

--- lib/helmsnap/generate.rb
+++ lib/helmsnap/generate.rb
@@ -45,6 +45,7 @@ class Helmsnap::Generate < Helmsnap::Service
     content.gsub!(/\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d+/, "2022-01-01 00:00:00.000")
     content.gsub!(/\d\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d/, "2022-01-01-00-00-00")
     content.gsub!(/\d\d\d\d-\d\d-\d\d-\d\d-\d\d/, "2022-01-01-00-00")
+    content.gsub!(/^[ \t]+$/, "")
     content.gsub!(/\n{3,}/, "\n\n")
     content.gsub!(/\n\n+---/, "\n---")
     content.gsub!(/\n+\z/, "\n")
